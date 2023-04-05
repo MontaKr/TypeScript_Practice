7a64cb16391c311c3e614b38df51ea102b7bf33e
임지훈 <zhoon95@gmail.com>
2023-04-05 16:53:04 +0900

    lecture2

---
+++ 코딩애플/lecture1/index.ts
@@ -0,0 +1,5 @@
+let 이름: string = "kim";
+let 회원들: string[] = ["민수", "철수"];
+let 등번호: number[] = [1, 2];
+let 회원1: { name: string; number: number } = { name: "민수", number: 1 };
+let 회원: string | number = 123;
